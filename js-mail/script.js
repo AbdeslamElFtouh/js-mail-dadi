2e095159cefae013104ee77d78aa6a5dc2f6272b
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-03-27 10:16:57 +0100

    FIX alcune correzioni dell' esercizio mail-list

--- js-mail/script.js
+++ js-mail/script.js
@@ -3,17 +3,23 @@
 // 3) controlla che sia nella lista di chi può accedere;
 // 4) stampa un messaggio con l'esito del controllo (suggerito ciclo FOR);
 
-let nomi_da_verificare = prompt('Ciao, inserisci qui il tuo nome');
-let nome_minuscolo = nomi_da_verificare.toLowerCase();
-
 const invitati = ['Marco','Sandro','Pietro','Alessandro','Riccardo','Pierluigi','Leone','Ugo','Stefano','Napoleone']
 
-for (let i = 0; i < invitati.length; i++) {
+let nomi_da_verificare = prompt('Ciao, inserisci qui il tuo nome');
+let trovato = false;
+
+for (let i = 0; trovato === true || i < invitati.length; i++) {
     if (invitati[i] === nomi_da_verificare) {
+        trovato = true;
         alert('Benvenuto'+ ' ' + nomi_da_verificare + '!');
-        break;
-    } else {
-        alert('Mi dispiace'+ ' ' + nomi_da_verificare + ', ' + 'ma non riesco a trovare il tuo nome...');
-        break;
-    }
+    } 
 }
+
+if (trovato === false) {
+        alert('Mi dispiace'+ ' ' + nomi_da_verificare + ', ' + 'ma non riesco a trovare il tuo nome...');
+    }
+
+    // Correzione: 
+    // 1) evito di usare 'break' perchè è un uscita forzata dal ciclo, piuttosto complico la condizione "if" 
+    //    aggiungendo di uscire dal ciclo se viene trovato il nome;
+    // 2) utilizzare variabile di supporto "trovato";
